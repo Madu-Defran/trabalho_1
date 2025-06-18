978df471032aac10247b8dd797205d40e80754fa
Madu-Defran <mariaeduardasilva178@gmail.com>
2025-06-17 22:39:02 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const botao = document.getElementById('botao');
-const mensagemDiv = document.getElementById('mensagem');
-
-botao.addEventListener('click', function() {
-    mensagemDiv.textContent = 'Se voce só é bom em uma coisa, torne-se mestre nela. Demon Slayer.';
-    mensagemDiv.style.display = 'block';    
-    
- });
